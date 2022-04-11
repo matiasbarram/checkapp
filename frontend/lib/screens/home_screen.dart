8bef1593389dad525a0e9acef6fb3884f93174f2
matiasbarram <matias.barra@alumnos.uach.cl>
2022-04-10 21:12:09 -0400

    container wave

--- frontend/lib/screens/home_screen.dart
+++ frontend/lib/screens/home_screen.dart
@@ -21,78 +21,123 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Container(
-            height: 300,
-            color: AppTheme.checkappPrim,
-            width: double.infinity,
+          ClipPath(
+            clipper: ClippingClass(),
             child: Container(
-              margin: const EdgeInsets.only(top: 20),
-              child: Column(
-                children: [
-                  const ScanQRButton(),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 50),
-                    child: Column(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            color: Colors.white,
-                            boxShadow: const [
-                              BoxShadow(
-                                  color: Color.fromRGBO(0, 0, 0, 0.05),
-                                  spreadRadius: 0,
-                                  blurRadius: 4,
-                                  offset: Offset(3, 4))
-                            ],
+              height: 320,
+              color: AppTheme.checkappPrim,
+              width: double.infinity,
+              child: Container(
+                margin: const EdgeInsets.only(top: 20),
+                child: Column(
+                  children: [
+                    const ScanQRButton(),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 50),
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: Colors.white,
+                              boxShadow: const [
+                                BoxShadow(
+                                    color: Color.fromRGBO(0, 0, 0, 0.05),
+                                    spreadRadius: 0,
+                                    blurRadius: 4,
+                                    offset: Offset(3, 4))
+                              ],
+                            ),
+                            width: double.infinity,
+                            height: 80,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: const [
+                                    Text('Llegada'),
+                                    Text('08:05 am')
+                                  ],
+                                ),
+                                const VerticalDivider(
+                                  width: 1,
+                                  thickness: 0.5,
+                                  color: AppTheme.checkApptextLight,
+                                ),
+                                Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: const [
+                                    Text('Llegada'),
+                                    Text('08:05 am')
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
-                          width: double.infinity,
-                          height: 80,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: const [
-                                  Text('Llegada'),
-                                  Text('08:05 am')
-                                ],
-                              ),
-                              const VerticalDivider(
-                                width: 1,
-                                thickness: 0.5,
-                                color: AppTheme.checkApptextLight,
-                              ),
-                              Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: const [
-                                  Text('Llegada'),
-                                  Text('08:05 am')
-                                ],
-                              ),
-                            ],
+                          const SizedBox(
+                            height: 10,
                           ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        const Text(
-                          '¿Tienes algún problema?',
-                          style: TextStyle(color: AppTheme.checkApptextLight),
-                        )
-                      ],
-                    ),
-                  )
-                ],
+                          const Text(
+                            '¿Tienes algún problema?',
+                            style: TextStyle(color: AppTheme.checkApptextLight),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+            width: double.infinity,
+            alignment: Alignment.topLeft,
+            child: Column(
+              children: const [Text('Así va tu asistencia de este mes')],
             ),
           )
         ],
       ),
     );
+  }
+}
+
+class ClippingClass extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(
+        0, size.height); //start path with this if you are making at bottom
+
+    var firstStart = Offset(size.width / 5, size.height);
+    //fist point of quadratic bezier curve
+    var firstEnd = Offset(size.width / 2.5, size.height - 35.0);
+    //second point of quadratic bezier curve
+    path.quadraticBezierTo(
+        firstStart.dx, firstStart.dy, firstEnd.dx, firstEnd.dy);
+
+    var secondStart =
+        Offset(size.width - (size.width / 3.24), size.height - 65);
+    //third point of quadratic bezier curve
+    var secondEnd = Offset(size.width, size.height);
+    //fourth point of quadratic bezier curve
+    path.quadraticBezierTo(
+        secondStart.dx, secondStart.dy, secondEnd.dx, secondEnd.dy);
+
+    path.lineTo(
+        size.width, 0); //end with this path if you are making wave at bottom
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
   }
 }
